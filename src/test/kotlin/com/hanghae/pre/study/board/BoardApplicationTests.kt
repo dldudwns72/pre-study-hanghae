6aa0262be0acf02686270936d714e7026c696047
dldudwns72 <dldudwns72@gmail.com>
2024-11-16 20:37:05 +0900

    first-commit

---
+++ src/test/kotlin/com/hanghae/pre/study/board/BoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hanghae.pre.study.board
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
